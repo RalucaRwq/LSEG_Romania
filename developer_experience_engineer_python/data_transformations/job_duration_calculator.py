893807b777a9adc4c48447ac9dd02c80ed4adf78
RalucaRwq <ciulavur@yahoo.com>
2025-04-11 02:24:58 +0300

    Add job duration report.

--- developer_experience_engineer_python/data_transformations/job_duration_calculator.py
+++ developer_experience_engineer_python/data_transformations/job_duration_calculator.py
@@ -50,6 +50,11 @@ def measure_duration(df):
                 status = 'warning'  # Job took longer than 5 minutes
             # Else mark it as empty
 
+        # Format start_time, end_time and duration to HH:MM:SS
+        start_time = str(start_time).split(' ')[-1]
+        end_time = str(end_time).split(' ')[-1] if end_time is not None else ''
+        duration = str(duration).split(' ')[-1] if end_time is not None else ''
+
         # Store duration and status in the dictionary
         durations_dict[pid] = {
             'job_description': job_description,
